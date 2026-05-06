f522ca60bb0b36699f5ea82469766b2d46e5f7d8
CarixStudio <studio.carix@gmail.com>
2026-05-06 06:31:53 +0100

    feat: fix email service

--- services/emailService.js
+++ services/emailService.js
@@ -49,7 +49,7 @@ export const emailService = {
       const { data, error } = await resend.events.send({
         event: eventName,
         email: email,
-        payload: dataFields
+        ...dataFields
       });
 
       if (error) {
